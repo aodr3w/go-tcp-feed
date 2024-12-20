831b493c600c1832583785c4437fdc07068c5878
AndrewOdiit <andrewodiit2020@gmail.com>
2024-12-20 19:47:53 +0300

    return len() of data as nextIdx from Write function to avoid self consumption

--- server/broacast.go
+++ server/broacast.go
@@ -5,6 +5,11 @@ import (
 	"sync"
 )
 
+//NOTES
+/*
+since reading has a faster turn over time than writing, we should read first
+and then attempt to write to the data slice in the handle connection function
+*/
 type Broadcast struct {
 	m    *sync.RWMutex
 	data [][]byte
@@ -17,10 +22,15 @@ func NewBroadCast() *Broadcast {
 	}
 }
 
-func (bc *Broadcast) Write(data []byte) {
+func (bc *Broadcast) Write(data []byte) int {
 	bc.m.Lock()
 	defer bc.m.Unlock()
 	bc.data = append(bc.data, data)
+	// Return the current length of the data slice, which represents the next index to read from.
+	// This ensures the writer (publisher) can avoid reading the message it just wrote.
+	// Consumers might encounter index errors if they try to read an index that is not yet written,
+	// but this is expected behavior and should be handled by the caller.
+	return len(bc.data)
 }
 
 func (bc *Broadcast) Read(idx int) ([]byte, error) {
