89f4c02fb37f426f5e118cf6492f5fafbec9a299
AndrewOdiit <andrewodiit2020@gmail.com>
2024-12-25 16:46:30 +0300

    add to and from bytes conversion functions with gob for Message Model

--- db/models.go
+++ db/models.go
@@ -1,6 +1,11 @@
 package db
 
-import "time"
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"time"
+)
 
 // represents a connected client
 type User struct {
@@ -14,4 +19,26 @@ type Message struct {
 	Name      string
 	Text      string
 	CreatedAt time.Time
+}
+
+func (m *Message) ToBytes() ([]byte, error) {
+	var buf bytes.Buffer
+	encoder := gob.NewEncoder(&buf)
+	err := encoder.Encode(m)
+	if err != nil {
+		return nil, fmt.Errorf("error encoding message: %w", err)
+	}
+	return buf.Bytes(), nil
+}
+
+// FromBytes converts a byte array to a Message struct using Gob.
+func FromBytes(data []byte) (*Message, error) {
+	var msg Message
+	buf := bytes.NewBuffer(data)
+	decoder := gob.NewDecoder(buf)
+	err := decoder.Decode(&msg)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding message: %w", err)
+	}
+	return &msg, nil
 }
