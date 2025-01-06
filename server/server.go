643c8d0478f826622c61f1912ca39d5a7537a14a
AndrewOdiit <andrewodiit2020@gmail.com>
2025-01-06 15:32:58 +0300

    add waitGroup to server.Start

--- server/server.go
+++ server/server.go
@@ -1,13 +1,25 @@
 package server
 
+import (
+	"log"
+	"sync"
+)
+
 func Start(writePort int, readPort int) {
+	log.Println("starting server...")
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
 	go func() {
 		s := NewService()
 		WriteMessages(writePort, s)
+		defer wg.Done()
 	}()
 
+	wg.Add(1)
 	go func() {
 		s := NewService()
 		ReadMessages(readPort, s)
+		wg.Done()
 	}()
+	wg.Wait()
 }
